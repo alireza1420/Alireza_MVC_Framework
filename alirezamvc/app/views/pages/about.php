e0e9f130308674aa38cbb5c87ad016b93927afb0
alireza1420 <74127671+alireza1420@users.noreply.github.com>
2022-07-25 23:36:52 +0430

    Add files via upload

---
+++ alirezamvc/app/views/pages/about.php
@@ -0,0 +1,3 @@
+<?php require_once(APPROOT."/views/inc/header.php"); ?>
+<?php require_once(APPROOT."/views/inc/footer.php"); ?>
+<h1><?php echo $data['title'];?></h1>
---
+++ alirezamvc/app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php require_once(APPROOT."/views/inc/header.php"); ?>
+<?php require_once(APPROOT."/views/inc/footer.php"); ?>
+<h1><?php echo $data['title'];?></h1>
+<p>This is the Alireza MVC Framework , please refer to documantation for more info about this Framework</p>
+
+
